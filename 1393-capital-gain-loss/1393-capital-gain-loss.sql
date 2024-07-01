38d59f8a06098607ddd74915ca12985e1c6528f6
JeremyKamLongbow <jeremy.kam@longbow.com.my>
2024-07-01 12:50:46 +0800

    Initial Commit - 1/7/2024

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-select stock_name, sum(case when operation = 'Buy' then price*-1 else price end) as capital_gain_loss from stocks group by stock_name
--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-SELECT customers.name as 'Customers' FROM customers WHERE customers.id NOT IN (SELECT customerid FROM orders);
